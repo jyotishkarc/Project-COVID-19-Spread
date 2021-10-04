bb250069f6fd4b68f399b87588610915e1ec09a9
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-10-05 02:07:36 +0530

    Aytijhya's code for forward regression suggested by Prof. Das

--- wb-district-wise-forward-regression-PKD.R
+++ wb-district-wise-forward-regression-PKD.R
@@ -1,7 +1,5 @@
 library(dplyr)
-library(ppcor)
-districts.df <- read.csv("G:/B.Stat/Projects/Gopal K. Basak/GKB Sir/Dataset/districts.csv")
-
+districts.df <-read.csv(file.choose(),header = TRUE)
 districts.df <- as.data.frame(districts.df)
 districts.conf <- districts.df %>% filter(State == "West Bengal")
 
@@ -12,8 +10,7 @@ uni <- districts.conf[districts.conf[,3] != "Unknown" ||
 uni <- uni[,c(1,3,4)]
 
 uni.dist <- setdiff(uni.dist, c("Unknown", "Other State"))
-
-X <- list()
+X=list()
 for (i in 1:length(uni.dist)) {
    X[[i]] <- uni[uni[,2] == uni.dist[i],3]
 }
@@ -23,6 +20,7 @@ M <- matrix(0, nrow = 503, ncol = 23)
 for (k in 1:23) {
    M[,k] <- c(rep(0, nrow(M)-length(X[[k]])), X[[k]])
 }
+#colnames(M)=uni.dist
 
 reg.list <- list()
 
@@ -33,42 +31,36 @@ districts.cleaned <- apply(M, 2, function(vec){
 
 districts.cleaned <- as.data.frame(districts.cleaned)
 
-Y <- districts.cleaned <- districts.cleaned[151:180,]
+Y=districts.cleaned <- districts.cleaned[151:180,]
 colnames(Y) <- uni.dist
 
 
+
+
+mat=list()
 for(i in 1:23){
    v <- cor(Y[,i],Y[,-i])
    reg <- matrix(0, ncol = 22, nrow = 1) %>% as.data.frame
    colnames(reg)[1] <- colnames(v)[which.max(abs(v))]
    reg[,1] <- max(abs(v))
    
-   m <- 2
    
-   for(j in 1:23){
-      a <- rep(0,23)
-      
-      if(j != i & !(colnames(Y)[j] %in% colnames(reg))){
-         a[j] <- pcor.test(Y[,i],Y[,j],
-                           Y[,which(colnames(Y) %in% colnames(reg))])$estimate
-      }
+   for(k in 2:22){
+      a <- matrix(0, ncol = 23, nrow = 1) %>% as.data.frame
+      colnames(a) <- uni.dist
+      for(j in 1:23){
          
-      if (m != 23) {
-         colnames(reg)[m] <- setdiff(colnames(v), colnames(reg))[which.max(abs(a))]
-         reg[,m] <- max(abs(a))
+         if(j != i & !(colnames(Y)[j] %in% colnames(reg))){
+            a[j] <- pcor.test(Y[,i],Y[,j],
+                              Y[,which(colnames(Y) %in% colnames(reg))])$estimate
+         }
       }
+      cat("a")
+      print(a)
+      colnames(reg)[k] = colnames(a)[which.max(abs(a))]
+      reg[,k] <- max(abs(a))
       
-      j <- j + 1
-      m <- m + 1
    }
-
-
-
-
-
-
-
-
-
-
-
+   mat[[i]]=reg
+   print(reg)
+}
