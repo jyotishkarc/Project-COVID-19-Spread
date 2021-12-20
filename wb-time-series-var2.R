4945d096e9542663a7c3b13a64e5b22b6dbaa41c
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-12-21 00:59:23 +0530

    Arranged the districts as per GKB sir said

--- wb-time-series-var2.R
+++ wb-time-series-var2.R
@@ -1,50 +1,72 @@
 
 library(dplyr)
-library("vars")
+library(vars)
+library(readxl)
 
 n <- 7
 
-districts.conf <- districts.df %>% filter(State == "West Bengal")
-
-districts.conf[,3] <- districts.conf[,3] %>% gsub(" ", ".", .)
-
-uni <- districts.conf[districts.conf[,3] != "Unknown" ||
-                         districts.conf[,3] != "Other.State" , ][,c(1,3,4)]
-
-uni.dist <- districts.conf$District %>%
-   unique() %>% 
-   setdiff(c("Unknown", "Other.State"))
-# gsub(" ", ".", .)
-
-X <- list()
-for (i in 1:length(uni.dist)) {
-   X[[i]] <- uni[uni[,2] == uni.dist[i],3]
+if(TRUE){
+   
+   path <- "D:/My Documents/R/R Codes/Project on Spread of COVID-19/Datasets/"
+   
+   districts.df <- read.csv(paste0(path,"districts.csv")) %>% as.data.frame()
+   districts.conf <- districts.df %>% filter(State == "West Bengal")
+   districts.conf[,3] <- districts.conf[,3] %>% gsub(" ", ".", .)
+   
+   arranged.districts.data <- read_excel(paste0(path,"arranged_districts.xlsx"))[,-1]
+   arranged.districts <- colnames(arranged.districts.data)
+   
+   uni <- districts.conf[districts.conf[,3] != "Unknown" ||
+                            districts.conf[,3] != "Other.State" , ][,c(1,3,4)]
+   
+   # uni.dist <- districts.conf$District %>%
+   #    unique() %>%
+   #    setdiff(c("Unknown", "Other.State"))
+   
+   uni.dist <- arranged.districts
+   
+   X <- list()
+   for (i in 1:length(uni.dist)) {
+      X[[i]] <- uni[uni[,2] == uni.dist[i],3]
+   }
+   
+   J <- districts.conf[,1] %>% unique() %>% length()
+   M <- matrix(0, nrow = J, ncol = 23)
+   
+   for (k in 1:23) {
+      M[,k] <- c(rep(0, nrow(M)-length(X[[k]])), X[[k]])
+   }
+   
+   districts.cleaned <- apply(M, 2, function(vec){
+      temp <- c(0, vec[-length(vec)])
+      return(vec - temp)
+   })
+   
+   districts.cleaned <- districts.cleaned %>% as.data.frame()
+   colnames(districts.cleaned) <- uni.dist
+   
+   #E <- districts.cleaned %>% select(arranged.districts)
 }
-
-J <- districts.conf[,1] %>% unique() %>% length()
-M <- matrix(0, nrow = J, ncol = 23)
-
-for (k in 1:23) {
-   M[,k] <- c(rep(0, nrow(M)-length(X[[k]])), X[[k]])
-}
-
-districts.cleaned <- apply(M, 2, function(vec){
-   temp <- c(0, vec[-length(vec)])
-   return(vec - temp)
-})
-
 
 
 G <- matrix(0, nrow = floor(J/n), ncol = 23)
 
 for(i in 1:23){
    for(k in 1:floor(J/n)){
-      G[k,i] <- sum(districts.cleaned[(n*k-n+1):(n*k),i])/n
+      # G[k,i] <- sum(districts.cleaned[(n*k-n+1):(n*k),i])/n
+      G[k,i] <- sum(districts.cleaned[(n*k-n+1):(n*k),i])
    }
 }
 
-G <- as.data.frame(G)
+G <- G %>% as.data.frame()
 colnames(G) <- uni.dist
 rownames(G) <- sapply(1:floor(J/n), function(val){paste0("w",val)})
-x= VAR(G, p=2, type="both")
+
+x <- VAR(G, p=2, type="both")
 summary(x)
+
+
+
+
+
+
