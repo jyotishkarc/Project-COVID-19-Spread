a66abd5aba7349333eb3f48611794deb4157fa87
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-03-16 03:48:24 +0530

    Added code for prediction in real-time

--- prediction-in-both-scale.R
+++ prediction-in-both-scale.R
@@ -5,7 +5,7 @@ library(vars)
 # districts.df <- as.data.frame(districts.df)
 
 path <- "D:/My Documents/R/R Codes/Project on Spread of COVID-19/Datasets/"
-path <- "/Users/aytijhyasaha/Desktop/projects/spread of covid/Project-COVID-19-Spread/Datasets/"
+# path <- "/Users/aytijhyasaha/Desktop/projects/spread of covid/Project-COVID-19-Spread/Datasets/"
 
 districts.df <- read.csv(paste0(path,"districts.csv")) %>% as.data.frame()
 
@@ -43,7 +43,9 @@ districts.cleaned <- districts.cleaned %>% apply(c(1,2), function(val){
    else return(log(val + 1))
 }) %>% as.data.frame()
 
-x <- VAR(districts.cleaned[1:400,], p = 2, type = "const")
+N=500
+
+x <- VAR(districts.cleaned[1:N,], p = 2, type = "const")
 L <- 23
 B_1 <- B_2 <- matrix(0, nrow = L, ncol = L)
 
@@ -55,13 +57,24 @@ for(i in 1:L){
 colnames(B_1) <- colnames(B_2) <- rownames(B_1) <- rownames(B_2) <- uni.dist
 
 
-var.pred = predict(x, n.ahead = nrow(districts.cleaned)-400)
-error=numeric(L)
+var.pred <- predict(x, n.ahead = nrow(districts.cleaned)-N)
+var.pred.usual <- lapply(var.pred$fcst, function(lst){
+   apply(lst, c(1,2), function(val){
+      return(exp(val) - 1)
+   })
+})
+
+error <- error.usual <- numeric(L)
 
 for (i in 1:L) 
-   for (j in 401:554) 
-      error[i]=error[i]+abs(var.pred$fcst[i][[1]][j-400,1]-districts.cleaned[j,i])/districts.cleaned[j,i]
+   for (j in (N+1):554) 
+      error[i]=error[i]+abs(var.pred$fcst[i][[1]][j-N,1]-districts.cleaned[j,i])
 
-error=error/154
-names(error)=uni.dist 
+for (i in 1:L) 
+   for (j in (N+1):554) 
+      error.usual[i]=error.usual[i]+abs(var.pred.usual[i][[1]][j-N,1]-exp(districts.cleaned[j,i]) -1)/(exp(districts.cleaned[j,i]) - 1)
+
+error=error/54
+error.usual=error.usual/54
+names(error) <- names(error.usual) <- uni.dist 
 error
