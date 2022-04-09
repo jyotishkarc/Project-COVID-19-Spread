f433ab82743166cb71a81d32f90facb52d897526
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-04-09 14:34:56 +0530

    Removed some redundant code chunks

--- prediction-in-both-scale.R
+++ prediction-in-both-scale.R
@@ -1,47 +1,9 @@
 library(dplyr)
 library(expm)
 library(vars)
-# districts.df <- read.csv(file.choose(),header = TRUE)
-# districts.df <- as.data.frame(districts.df)
 
-path <- "D:/My Documents/R/R Codes/Project on Spread of COVID-19/Datasets/"
-# path <- "/Users/aytijhyasaha/Desktop/projects/spread of covid/Project-COVID-19-Spread/Datasets/"
-
-districts.df <- read.csv(paste0(path,"districts.csv")) %>% as.data.frame()
-
-districts.conf <- districts.df %>% filter(State == "West Bengal")
-
-uni.dist <- unique(districts.conf$District)
-
-uni <- districts.conf[districts.conf[,3] != "Unknown" ||
-                         districts.conf[,3] != "Other State" , ]
-uni <- uni[,c(1,3,4)]
-
-uni.dist <- setdiff(uni.dist, c("Unknown", "Other State"))
-
-for (i in 1:length(uni.dist)) {
-   X[[i]] <- uni[uni[,2] == uni.dist[i],3]
-}
-
-M <- matrix(0, nrow = 554, ncol = 23)
-
-for (k in 1:23) {
-   M[,k] <- c(rep(0, nrow(M)-length(X[[k]])), X[[k]])
-}
-
-reg.list <- list()
-
-districts.cleaned <- apply(M, 2, function(vec){
-   temp <- c(0, vec[-length(vec)])
-   return(vec - temp)
-})
-
-districts.cleaned <- districts.cleaned %>% apply(c(1,2), function(val){
-   if(val < 0){
-      return(0)
-   }
-   else return(log(val + 1))
-}) %>% as.data.frame()
+# path.preamble <- paste0(getwd(),"/preamble.R")
+# source(path.preamble)
 
 N=500
 
